8288b6f6071808ab792e303b330efec177dc5cb2
Praveen K M <mvpl1430@gmail.com>
2026-05-31 19:25:30 +0530

    Hardcode Supabase keys for demo

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(
+  'https://vnuufbqskgxkpvckqgmd.supabase.co',
+  ' 	eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZudXVmYnFza2d4a3B2Y2txZ21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMTE3NTMsImV4cCI6MjA5NTc4Nzc1M30.ZaOxgosWZdt2RQoB10o2n-nLKHospdmAIdSVgyGxm1U'
+)
