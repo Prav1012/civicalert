7122d4bc862371299eb11c4a0e1cac5c2fda796a
Praveen K M <mvpl1430@gmail.com>
2026-06-01 01:19:52 +0530

    Hardcode Supabase for demo

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://vnuufbqskgxkpvckqgmd.supabase.co',
-  ' 	eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZudXVmYnFza2d4a3B2Y2txZ21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMTE3NTMsImV4cCI6MjA5NTc4Nzc1M30.ZaOxgosWZdt2RQoB10o2n-nLKHospdmAIdSVgyGxm1U'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZudXVmYnFza2d4a3B2Y2txZ21kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMTE3NTMsImV4cCI6MjA5NTc4Nzc1M30.ZaOxgosWZdt2RQoB10o2n-nLKHospdmAIdSVgyGxm1U'
 )
